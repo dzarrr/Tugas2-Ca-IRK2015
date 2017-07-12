cde7458f80b50c1f72512343848a30ce639320b3
dzarrr <dzarbelahanifa@gmail.com>
2017-07-12 23:00:05 +0700

    Update bigNumber library header

--- bigNumber.h
+++ bigNumber.h
@@ -9,9 +9,11 @@ public:
 	bigNumber(char* number);
 	bigNumber(const bigNumber& n1);
 	printbigNum();
-	int bigNumberIndex(bigNumber n1, int index); //Untuk mendapatkan digit ke-index dari suatu bigNumber
-	bigNumber& addbigNum(bigNumber n1, bigNumber n2);
-	bigNumber subtractbigNum(bigNumber n1, bigNumber n2);
+	int bigNumberIndex(int index); //Untuk mendapatkan digit ke-index dari suatu bigNumber
+	addbigNum(bigNumber n2);
+	subtractbigNum(bigNumber n2);
+	bool isGreaterThan(bigNumber n2);
+	bool isSmallerThan(bigNumber n2);
 
 private:
 	int* data;

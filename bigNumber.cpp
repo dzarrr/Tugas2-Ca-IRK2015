78a7dd43e85c334e4d5620b31f9a03b91a14ebc2
dzarrr <dzarbelahanifa@gmail.com>
2017-07-12 22:59:30 +0700

    Update bigNumber library
    menambahkan operasi pengurangan serta relasional

--- bigNumber.cpp
+++ bigNumber.cpp
@@ -36,20 +36,20 @@ bigNumber::printbigNum(){
 	cout << endl;
 }
 
-int bigNumber::bigNumberIndex(bigNumber n1, int index){
-	return n1.data[index];
+int bigNumber::bigNumberIndex(int index){
+	return data[index];
 }
 
-bigNumber& bigNumber::addbigNum(bigNumber n1, bigNumber n2){
+bigNumber::addbigNum(bigNumber n2){
 	char temp[25];
 	int length = 0;
 	int i = 0;
 	int result = 0;
 	bool carry = false;
 
-	while ((i < n1.digit) || (i < n2.digit)){
-		if ((i < n1.digit) && (i < n2.digit)){
-			result = bigNumberIndex(n1,i) + bigNumberIndex(n2,i);
+	while ((i < digit) || (i < n2.digit)){
+		if ((i < digit) && (i < n2.digit)){
+			result = bigNumberIndex(i) + n2.bigNumberIndex(i);
 			if (carry){
 				result++;
 			}
@@ -59,23 +59,143 @@ bigNumber& bigNumber::addbigNum(bigNumber n1, bigNumber n2){
 			} else {
 				carry = false;
 			}
-		} else if ((i < n1.digit) && (i >= n2.digit)){
-			result = bigNumberIndex(n1,i);
-		} else if ((i >= n1.digit) && ( i < n2.digit)){
-			result = bigNumberIndex(n2,i);
+		} else if ((i < digit) && (i >= n2.digit)){
+			result = bigNumberIndex(i);
+		} else if ((i >= digit) && ( i < n2.digit)){
+			result = n2.bigNumberIndex(i);
 		} temp[i] = result + '0';
 		  i++;
 		  length++;
 	}
-
 	char reversed[25];
 	int indeks = 0;
 	for (int i = length-1; i >= 0; i--){
 		reversed[indeks] = temp[i];
-		cout << reversed[indeks];
 		indeks++;
 	}
-	cout << endl;
-	bigNumber res(reversed);
-	return res;
+	reversed[indeks] = '\0';
+
+	delete [] data;
+	data = new int [25];
+
+	i = 0;
+	int tempint[25];
+	while (reversed[i]!='\0'){
+		tempint[i] = reversed[i] - '0';
+		i++;
+	}
+	int tempdigit = i;
+
+	indeks = 0;
+
+	for (int i = tempdigit-1; i >= 0; i--){
+		data[indeks] = tempint[i] ;
+		indeks++;
+	}
+	digit = tempdigit;
+	cout << digit << endl;
+}
+
+
+bigNumber::subtractbigNum(bigNumber n2){
+	char temp[25];
+	int length = 0;
+	int i = 0;
+	int result = 0;
+	bool carry = false;
+
+	while ((i < digit) || (i < n2.digit)){
+		if ((i < digit) && (i < n2.digit)){
+			if (bigNumberIndex(i) < n2.bigNumberIndex(i)){
+				result = (bigNumberIndex(i)+10) - n2.bigNumberIndex(i);
+				if (carry) result --;
+				carry = true;
+			} else {
+				result = bigNumberIndex(i) - n2.bigNumberIndex(i);
+				if (carry) result--;
+				carry = false;
+			} 
+		} else if ((i < digit) && (i >= n2.digit)){
+			result = bigNumberIndex(i);
+		} else if ((i >= digit) && ( i < n2.digit)){
+			result = n2.bigNumberIndex(i);
+		} 
+		  temp[i] = result + '0';
+		  i++;	
+		  length++;
+	}
+	char reversed[25];
+	int indeks = 0;
+	for (int i = length-1; i >= 0; i--){
+		reversed[indeks] = temp[i];
+		indeks++;
+	}
+	reversed[indeks] = '\0';
+	
+	delete [] data;
+	data = new int [25];
+
+	i = 0;
+	int tempint[25];
+	while (reversed[i]!='\0'){
+		tempint[i] = reversed[i] - '0';
+		i++;
+	}
+	int tempdigit = i;
+
+	indeks = 0;
+
+	for (int i = tempdigit-1; i >= 0; i--){
+		data[indeks] = tempint[i] ;
+		indeks++;
+	}
+	digit = tempdigit;
+}
+
+bool bigNumber :: isGreaterThan (bigNumber n2){
+	if (digit > n2.digit){
+		return (true);
+	} else if (digit < n2.digit){
+		return (false);
+	} else {
+		int i = digit-1;
+		bool cek = false;
+		bool found = false;
+		while ((i >= 0) && (!found)){
+			if (bigNumberIndex(i) > n2.bigNumberIndex(i)){
+				cek = true;
+				found = true;
+			} else if (bigNumberIndex(i) < n2.bigNumberIndex(i)){
+				cek = false;
+				found = true;
+			} else{
+				i--;
+			}
+		}
+		return (cek);
+	}
+}
+
+bool bigNumber::isSmallerThan (bigNumber n2){
+	if (digit < n2.digit){
+		return (true);
+	} else if (digit > n2.digit){
+		return (false);
+	} else {
+		int i = digit-1;
+		bool cek = false;
+		bool found = false;
+		while ((i >= 0) && (!found)){
+			if (bigNumberIndex(i) < n2.bigNumberIndex(i)){
+				cek = true;
+				found = true;
+			} else if (bigNumberIndex(i) > n2.bigNumberIndex(i)){
+				cek = false;
+				found = true;
+			} else{
+				i--;
+			}
+		}
+		return (cek);
+	}
 }
